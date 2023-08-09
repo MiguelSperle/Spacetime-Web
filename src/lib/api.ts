c14bdc5c7775addf83debce40c019af92c004e14
MiguelSperle <miguelsperle205@yahoo.com>
2023-08-09 19:16:59 -0300

    UPDATED

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
-  headers: {
-    'X-Request-Origin': 'web', // passando o conteúdo do (X-Request-Origin) = web, para fazer a verificação lá no backend
-  },
+  // headers: {
+  //   'X-Request-Origin': 'web', // passando o conteúdo do (X-Request-Origin) = web, para fazer a verificação lá no backend
+  // },
 })
